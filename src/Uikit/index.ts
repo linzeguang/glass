e161cc9280aec8a797f037b87bfe0713938a2056
linzeguang <15817289730@163.com>
2022-07-25 18:41:25 +0800

    clear code

--- src/Uikit/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Box';
-export * from './Button';
-export * from './Flex';
-export * from './Grid';
-export { default as ResetCSS } from './ResetCSS';
-export * from './theme';
--- src/Uikit/theme/utils.ts
+++
@@ -1,4 +0,0 @@
-import type { Palette } from '@emotion/react';
-import Color from 'color';
-
-export const toAlpha: Palette['toAlpha'] = (color, alpha) => Color(color).alpha(alpha).toString();
